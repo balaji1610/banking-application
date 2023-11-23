03a40e23a3ad8efd6621179f28d66bd76d0e09cc
balaji1610 <balajiece1610@gmail.com>
2023-11-23 13:11:56 +0530

    material_ui_install

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Crud from "../components/Crud";
+import Button from "@mui/material/Button";
+
 export default function index() {
   return (
     <div>
       <Crud />
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
     </div>
   );
 }
