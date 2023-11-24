d802827bd2fa1fb8fa43eceb27330958bd8e4bc4
balaji1610 <balajiece1610@gmail.com>
2023-11-24 17:44:54 +0530

    logout_btn

--- Container/Dashboard/LogoutButton.tsx
+++ Container/Dashboard/LogoutButton.tsx
@@ -5,11 +5,21 @@ export default function LogoutButton({ handleLogout, getId }) {
     <div className={LogoutButtonStyle.LogoutButtonStyle_flex}>
       <div>
         {" "}
-        <p> {getId}</p>
+        <div className={LogoutButtonStyle.user_imageFlex}>
+          <div>
+            <img
+              width="54"
+              height="54"
+              src="https://img.icons8.com/3d-fluency/94/administrator-male--v1.png"
+              alt="administrator-male--v1"
+            />
+          </div>
+          <div className={LogoutButtonStyle.user_name}>{getId}</div>
+        </div>
       </div>
       <div>
         {" "}
-        <Button onClick={handleLogout} variant="contained" color="primary">
+        <Button onClick={handleLogout} variant="contained" color="error">
           Log out
         </Button>
       </div>
