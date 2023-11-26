615ac4e5ccbfe24e35cb9826b8fee80de63145b1
balaji1610 <balajiece1610@gmail.com>
2023-11-26 22:50:41 +0530

    Sidebar

--- Container/Dashboard/Menu1.tsx
+++ Container/Dashboard/Menu1.tsx
@@ -1,3 +1,3 @@
 export default function Menu1() {
-  return <div>01</div>;
+  return <div>Dashboards</div>;
 }
--- Container/Dashboard/Sidebar.tsx
+++ Container/Dashboard/Sidebar.tsx
@@ -4,8 +4,11 @@ import SidebarStyle from "../../styles/Sidebar.module.css";
 import { useContext } from "react";
 import { ApplicationProps } from "../../ContextAPI/Context";
 import HomeIcon from "@mui/icons-material/Home";
+import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { Home, Dashboard } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useRouter } from "next/router";
 export default function Sidebar() {
   const [Togglemenu, setTogglemenu] = useState(true);
   const { RouterPath } = useContext(ApplicationProps);
@@ -13,7 +16,11 @@ export default function Sidebar() {
     event.preventDefault();
     setTogglemenu(!Togglemenu);
   };
+  const router = useRouter();
 
+  const ClickRouter = () => {
+    router.push("/aboutme");
+  };
   return (
     <div>
       <div onClick={MenuClick} className={SidebarStyle.Hamburger_menu}>
@@ -22,8 +29,8 @@ export default function Sidebar() {
           <MenuIcon />
         </IconButton>
       </div>
-      <div className={SidebarStyle.menu_list_parent}>
-        <div>
+      <div>
+        {/* <div>
           <div>
             <Link to={`/login/${RouterPath}`}>Home</Link>
           </div>
@@ -32,7 +39,55 @@ export default function Sidebar() {
             {" "}
             <Link to="/aboutme">Dashboard</Link>
           </div>
-        </div>
+        </div> */}
+        <List>
+          {Togglemenu ? (
+            <div className={SidebarStyle.menu_content_list}>
+              <Link
+                to={`/login/${RouterPath}`}
+                className={SidebarStyle.link_router}
+              >
+                <ListItem>
+                  <ListItemIcon>
+                    <Home />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItem>
+              </Link>{" "}
+              <Link to="/aboutme" className={SidebarStyle.link_router}>
+                {" "}
+                <ListItem>
+                  <ListItemIcon>
+                    <Dashboard />
+                  </ListItemIcon>
+                  <ListItemText primary="Dashboard" />
+                </ListItem>
+              </Link>
+            </div>
+          ) : (
+            <div className={SidebarStyle.icons_list}>
+              <Link
+                to={`/login/${RouterPath}`}
+                className={SidebarStyle.link_router}
+              >
+                {" "}
+                <ListItem button>
+                  <ListItemIcon>
+                    <Home />
+                  </ListItemIcon>
+                </ListItem>
+              </Link>
+              <Link to="/aboutme" className={SidebarStyle.link_router}>
+                <ListItem button>
+                  {" "}
+                  <ListItemIcon>
+                    <Dashboard />
+                  </ListItemIcon>
+                </ListItem>
+              </Link>
+            </div>
+          )}
+        </List>
       </div>
     </div>
   );
