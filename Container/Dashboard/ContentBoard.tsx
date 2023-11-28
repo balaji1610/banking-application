b772255948fbe187ab578fe54cdc621383dffe7e
balaji1610 <balajiece1610@gmail.com>
2023-11-28 11:18:55 +0530

    add_style

--- Container/Dashboard/ContentBoard.tsx
+++ Container/Dashboard/ContentBoard.tsx
@@ -1,5 +1,5 @@
 import ContenBoardStyle from "../../styles/ContenBoard.module.css";
-
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { useState } from "react";
 import ModelComp from "../../components/Model_Comp";
 import Table from "./Table";
@@ -17,13 +17,9 @@ export default function ContentBoard() {
     <div>
       <div className={ContenBoardStyle.AddBtn_style}>
         <div onClick={handleModelOpen}>
-          <img
-            width="75"
-            height="75"
-            src="https://img.icons8.com/plasticine/100/plus-2-math.png"
-            alt="add--v1"
-            title="Add"
-            style={{ cursor: "pointer" }}
+          <AddCircleOutlineIcon
+            fontSize="large"
+            style={{ cursor: "pointer", color: "#ffffff", fontSize: "3rem" }}
           />
         </div>
       </div>
