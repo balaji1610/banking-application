ea171dd784cb77008f2a58e98b1e1b83e62eeba0
balaji1610 <balajiece1610@gmail.com>
2023-11-29 10:43:48 +0530

    add_banking_style

--- Container/Banking/BankingPage.tsx
+++ Container/Banking/BankingPage.tsx
@@ -2,20 +2,18 @@ import { Button } from "@mui/material";
 import { ApplicationProps } from "../../ContextAPI/Context";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import BankingPageStyle from "../../styles/BankingPageStyle";
 export default function BankingPage() {
   const { RouterPath, ViewGetData } = useContext(ApplicationProps);
 
-  console.log(ViewGetData, "ViewGetData");
   return (
-    <div>
+    <div style={BankingPageStyle.parent}>
       <Link to={`/login/${RouterPath}`}>
         {" "}
         <Button variant="contained" color="error">
           Back
         </Button>
       </Link>
-
-      <h1>BankingPage</h1>
     </div>
   );
 }
---
+++ styles/BankingPageStyle.ts
@@ -0,0 +1,10 @@
+const BankingPageStyle = {
+  parent: {
+    backgroundColor: "#ffffff",
+    height: "100%",
+    border: "1px solid #000000",
+    borderRadius: "13px",
+  },
+};
+
+export default BankingPageStyle;
